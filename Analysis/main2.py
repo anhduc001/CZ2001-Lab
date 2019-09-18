230f5e143533e6840fe1287920edd9540bd531a5
archery2000 <ongyuhann@hotmail.com>
2019-09-19 02:00:02 +0800

    [Feat] Added some new tests

---
+++ Analysis/main2.py
@@ -0,0 +1,33 @@
+import pandas
+from matplotlib import pyplot as plt
+
+filepath = "D:\\Documents\\NTU\\Year2\\Sem1\\CZ2001 Algorithms\\CZ2001-Lab\\Lab 2\\"
+rangemax = 25
+
+d = pandas.DataFrame(pandas.read_csv(filepath+"linearprobebad.csv", header=None))
+d = d.transpose()
+d.columns = d.iloc[0]
+d = d.drop(0)
+arr = []
+yErr = []
+x = range(1, 100)
+for i in d:
+    arr.append(d[i].values.mean())
+    yErr.append(d[i].values.std())
+plt.errorbar(x[:rangemax], arr[:rangemax], yerr=yErr[:rangemax], fmt="r.")
+
+e = pandas.DataFrame(pandas.read_csv(filepath+"doublehashbad.csv", header=None))
+e = e.transpose()
+e.columns = e.iloc[0]
+e = e.drop(0)
+arre = []
+yErre = []
+for i in e:
+    arre.append(e[i].values.mean())
+    yErre.append(e[i].values.std())
+plt.errorbar(x[:rangemax], arre[:rangemax], yerr=yErre[:rangemax], fmt="b.")
+plt.xlabel("Load factor")
+plt.ylabel("Time in microseconds")
+plt.title("[Failure] Linear Probe vs Double Hashing (Table Size: 1009, Query Size: 1000)")
+plt.legend(labels=["linear", "double"], loc='upper center', bbox_to_anchor=(0.5, -0.2))
+plt.savefig("failure{}.png".format(rangemax), bbox_inches="tight")
---
+++ Lab 2/main2.cpp
@@ -0,0 +1,199 @@
+#include "fstream"
+#include "iostream"
+#include "chrono"
+#include "random"
+#include "vector"
+
+using namespace std;
+
+const int testFreq = 1000;
+const int testSize = 1000;
+
+class HashTable{
+    public:
+        virtual bool insert(int item){return false;};
+        virtual bool contains(int item){return false;};
+        void print(){
+            for(int i = 0; i < hashtableSize; i++){
+                if(table[i] != 0){
+                    cout << table[i] << "\n";
+                }
+            }
+        }
+        static const int hashtableSize = 1009;
+protected:
+        int table[hashtableSize] = {0};
+};
+class LinearProbe: public HashTable{
+	private:
+		int hashFunction(int item){
+			return (61*item) % hashtableSize;
+		};
+	public:
+		bool insert(int item) override{
+			int base_index = hashFunction(item);
+			int index = base_index;
+			while(table[index] != 0){
+				index++;
+				if(index >= hashtableSize) index = index % hashtableSize;
+				if(index == base_index) return false;
+			}
+			table[index] = item;
+			return true;
+		};
+		bool contains(int item) override{
+			int base_index = hashFunction(item);
+			int index = base_index;
+			while(table[index] != 0){
+				if(table[index] == item) return true;
+				index++;
+				if(index >= hashtableSize) index = index % hashtableSize;
+				if(index == base_index) return false;
+			}
+			return false;
+		};
+};
+class DoubleHash: public HashTable{
+	private:
+		int hashFunction1(int item){
+			return (61*item) % hashtableSize;
+		};
+		int hashFunction2(int item){
+			return (item % 37) + 1;
+		};
+	public:
+		bool insert(int item) override{
+			int base_index = hashFunction1(item);
+			int offset = hashFunction2(item);
+			int index = base_index;
+			while(table[index] != 0){
+				index += offset;
+				if(index >= hashtableSize) index = index % hashtableSize;
+				if(index == base_index) return false;
+			}
+			table[index] = item;
+			return true;
+		}
+		bool contains(int item) override{
+			int base_index = hashFunction1(item);
+			int offset = hashFunction2(item);
+			int index = base_index;
+			while(table[index] != 0){
+				if(table[index] == item) return true;
+				index += offset;
+				if(index >= hashtableSize) index = index % hashtableSize;
+				if(index == base_index) return false;
+			}
+			return false;
+		}
+};
+
+vector<int> GenerateRandomNumbers(int num, vector<int>* exist){
+    DoubleHash existing;
+    if(exist != nullptr){
+        for(int i:*exist){
+            existing.insert(i);
+        }
+    }
+    DoubleHash table;
+    if(num > table.hashtableSize){
+        num = table.hashtableSize;
+    }
+    vector<int> ret;
+    random_device rd;     // only used once to initialise (seed) engine
+    mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
+    uniform_int_distribution<int> uni(1000000,9999999); // guaranteed unbiased
+    for(int i = 0; i < num; i++){
+        ret.emplace_back(uni(rng));
+        if(!table.contains(ret[i]) && !existing.contains(ret[i])){
+            table.insert(ret[i]);
+        }else{
+            ret.erase(ret.end()-1);
+            i--;
+        }
+    }
+    return ret;
+}
+
+vector<int> GenerateRandomSubset(int num, vector<int>* exist){
+    vector<int> ret;
+    random_device rd;     // only used once to initialise (seed) engine
+    mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
+    uniform_int_distribution<int> uni(0,exist->size()); // guaranteed unbiased
+    for(int i = 0; i < num; i++){
+        ret.emplace_back((*exist)[uni(rd)]);
+    }
+    return ret;
+}
+
+long test(const vector<int>& testArgs, const vector<int>& search, HashTable* table){
+    for(int testArg : testArgs){
+        table->insert(testArg);
+    }
+    auto start = chrono::steady_clock::now();
+    for(int searchArg : search){
+            table->contains(searchArg);
+    }
+    auto end = chrono::steady_clock::now();
+//    cout << "Elapsed time in microseconds : "
+//         << chrono::duration_cast<chrono::microseconds>(end - start).count()
+//         << " µs" << endl;
+    return chrono::duration_cast<chrono::microseconds>(end - start).count();
+}
+
+int main()
+{
+    int i;
+    ofstream lin;
+    lin.open("linearprobe.csv");
+    ofstream dbl;
+    dbl.open("doublehash.csv");
+
+    cout << std::boolalpha;
+    //Load Factor Loop
+    for(int i = 1; i < 100; i++){
+        lin << i;
+        dbl << i;
+        for(int j = 0; j < testFreq; j++){
+            DoubleHash* dblhash = new DoubleHash;
+            LinearProbe* linearp = new LinearProbe;
+            vector<int> sample = GenerateRandomNumbers(HashTable::hashtableSize / 100 * i, NULL);
+            vector<int> success = GenerateRandomSubset(testSize, &sample);
+            lin << "," << test(sample, success, linearp);
+            dbl << "," << test(sample, success, dblhash);
+        }
+        if(i < 99){
+            lin << endl;
+            dbl << endl;
+        }
+    }
+    lin.close();
+    dbl.close();
+
+
+    ofstream linb;
+    linb.open("linearprobebad.csv");
+    ofstream dblb;
+    dblb.open("doublehashbad.csv");
+    // Worst Case
+    for(int i = 1; i < 100; i++){
+        linb << i;
+        dblb << i;
+        for(int j = 0; j < testFreq; j++){
+            DoubleHash* dblhash = new DoubleHash;
+            LinearProbe* linearp = new LinearProbe;
+            vector<int> sample = GenerateRandomNumbers(HashTable::hashtableSize / 100 * i, NULL);
+            vector<int> failed = GenerateRandomNumbers(HashTable::hashtableSize / 100 * i, NULL);
+            linb << "," << test(sample, failed, linearp);
+            dblb << "," << test(sample, failed, dblhash);
+        }
+        if(i < 99){
+            linb << endl;
+            dblb << endl;
+        }
+    }
+    linb.close();
+    dblb.close();
+    cin >> i;
+    return 0;
+}
