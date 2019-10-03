6846ac5634f44fb483bb6de8ca1413d0dc3c6e52
Ta Anh Duc <43318919+anhduc001@users.noreply.github.com>
2019-10-03 16:08:15 +0800

    Add files via upload

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <random>
+#include <set>
+#include <algorithm>
+#include <chrono>
+#include <fstream>
 
 using namespace std;
 
+//Maximum size of the vector
+const int maxSize = 1000000;
+//Number of tests per size
+const int testFreq = 10;
+
+//Merge function
 void merge(std::vector<int>&v, int first, int mid, int last)
 {
     std::vector<int> temp(v.size());
@@ -34,8 +45,9 @@ void merge(std::vector<int>&v, int first, int mid, int last)
     }
 }
 
+//Implementation of mergeSort
 void mergeSort(std::vector<int>&v, int first, int last){
-    if (first < last){
+    if (last - first > 0){
         int mid = first + (last - first) / 2;
         mergeSort(v, first, mid);
         mergeSort(v, mid + 1, last);
@@ -43,6 +55,7 @@ void mergeSort(std::vector<int>&v, int first, int last){
     }
 }
 
+//Implementation of insertionSort
 void insertionSort(vector<int>&v, int first, int last) {
     int i, j, cur;
     for (i = first; i <= last; i++)
@@ -59,9 +72,10 @@ void insertionSort(vector<int>&v, int first, int last) {
     }
 }
 
+//Implementation of timSort
 void timSort(std::vector<int>&v, int first, int last, int S) {
     if (last - first > S) {
-        int mid = (first + last) / 2;
+        int mid = first + (last - first) / 2;
         timSort(v, first, mid, S);
         timSort(v, mid + 1, last, S);
         merge(v, first, mid ,last);
@@ -70,24 +84,65 @@ void timSort(std::vector<int>&v, int first, int last, int S) {
         insertionSort(v, first, last);
     }
 }
-void reverseVector(std::vector<int>&v) {
-    std::vector<int> temp(v.size());
-    for(int i = 0; i < v.size(); i++)
+
+//Randomly generates a vector of n unordered integers
+vector<int> generateRandomInput(int n) {
+    set<int> ret;
+    random_device rd;
+    mt19937 rng(rd());
+    uniform_int_distribution<int> uni(0, n-1);
+    while(ret.size() != n)
     {
-        temp[i] = v[v.size() - i - 1];
+        ret.insert(uni(rng));
     }
-    for(int i = 0; i < v.size(); i++)
-    {
-        v[i] = temp[i];
-    }
+    vector<int> v;
+    v.assign(ret.begin(), ret.end());
+    shuffle (v.begin(), v.end(), rng);
+    return v;
 }
 
-int main() {
-    std::vector<int>v = {0,1,5,6,2,9,7,3};
-    timSort(v, 0, v.size()-1, 2);
-    for (int i = 0; i < v.size(); i++) {
-        std::cout << v[i] << ' ';
+//Test the execution time with different-size vectors
+long testSizeInput(vector<int> v, int choice){
+    if (choice == 0)
+    {
+        auto start = chrono::steady_clock::now();
+        timSort(v, 0, v.size() - 1, 500);
+        auto end = chrono::steady_clock::now();
+        return chrono::duration_cast<chrono::microseconds>(end - start).count();
     }
-    std::cout << "Hello, World!" << std::endl;
+    else if (choice == 1)
+    {
+        auto start = chrono::steady_clock::now();
+        mergeSort(v, 0, v.size() - 1);
+        auto end = chrono::steady_clock::now();
+        return chrono::duration_cast<chrono::microseconds>(end - start).count();
+    }
+    else
+        return 0;
+
+}
+
+
+int main() {
+
+    //Open file
+    ofstream size;
+    size.open("testSizeInput.csv");
+
+    //Size Loop
+    for(int i = 1000; i <= maxSize; i += 1000)
+    {
+        size << i;
+        //Testing Loop, with 0 for timSort and 1 for mergeSort
+        for (int j = 0; j < testFreq; j++) {
+            std::vector<int> v = generateRandomInput(i);
+            size << "," << testSizeInput(v, 0);
+            size << "," << testSizeInput(v, 1);
+            size << "\n";
+        }
+    }
+
+    size.close();
+    cout << "Finish!!" << endl;
     return 0;
 }
