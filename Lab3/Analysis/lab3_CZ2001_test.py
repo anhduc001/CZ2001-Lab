3cdac460f88b0f36a63a5db4051a85e3b8ec666b
saobangmath <42882997+saobangmath@users.noreply.github.com>
2019-10-24 10:38:54 +0800

    Adding analysing for Key Comparison figure

--- Lab3/Analysis/lab3_CZ2001_test.py
+++ Lab3/Analysis/lab3_CZ2001_test.py
@@ -13,15 +13,25 @@ def SValuePlot(df):
     df = df.set_index("S")
     df.plot()
     plt.ylabel("Time")
-    plt.xlabel("S")
+    plt.xlabel("Size")
     plt.title("Time execution with different S values")
     plt.savefig("TestValue.png")
+
+def KeyComparisons(df):
+    df = df.set_index("Size")
+    df.plot()
+    plt.ylabel("KeyComparisons")
+    plt.xlabel("Size")
+    plt.title("Key comparisons between Timsort and MergeSort")
+    plt.savefig("KeyComparisons.png")
 
 if __name__ == "__main__":
     data_file = "C:\\Users\\v\\source\\repos\\CZ2001-Lab\\Lab3\\"
     # Read CSV
     df1 = pd.read_csv(data_file + "testSizeInput.csv")
     df2 = pd.read_csv(data_file + "testValueS.csv")
+    df3 = pd.read_csv(data_file + "keyComparison.csv")
     SizeInputPlot(df1)
     SValuePlot(df2)
+    KeyComparisons(df3)
     plt.show()
